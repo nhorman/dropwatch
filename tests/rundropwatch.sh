c9ff0fc856c067603d8b6865aca52ef20774be32
Neil Horman <nhorman@tuxdriver.com>
2018-05-15 13:19:30 -0400

    Add make check run to travis ci
    Add the self tests to travis ci
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- tests/rundropwatch.sh
+++ tests/rundropwatch.sh
@@ -2,3 +2,15 @@
 
 echo -e "set alertlimit 1\nstart\nstop" | ../src/dropwatch -l kas
 
+if [ $? -ne 0 ]
+then
+	grep -q NET_DM ./rundropwatch.sh.log
+	if [ $? -eq 0 ]
+	then
+		# This platform doesn't support NET_DM, skip this test
+		exit 77
+	fi
+fi
+
+
+
