83a2b4e9bfe5ad26f59c15da5f77e86fbc0a6701
Neil Horman <nhorman@tuxdriver.com>
2019-11-18 08:28:38 -0500

    Attach SPDX license identifiers to all source files
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- src/lookup_bfd.c
+++ src/lookup_bfd.c
@@ -1,20 +1,6 @@
 /*
  * Copyright (C) 2009, Neil Horman <nhorman@redhat.com>
- *
- * This program file is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program in a file named COPYING; if not, write to the
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 /*
--- src/net_dropmon.h
+++ src/net_dropmon.h
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #ifndef __NET_DROPMON_H
 #define __NET_DROPMON_H
 
